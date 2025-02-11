9acbc770b0c91408d89cc7851f6e6e39edd139c8
unknown <ian.marcelo.perez@gmail.com>
2025-02-11 15:08:19 -0500

    Panel de Dificultad

--- Assets/MenuManager.cs
+++ Assets/MenuManager.cs
@@ -6,43 +6,51 @@ public class MenuManager : MonoBehaviour
     public GameObject MenuPanel;
     public GameObject opcionesPanel;
     public GameObject creditosPanel;
+    public GameObject dificultadPanel;
     public string CambiodeScena;
     private bool menuisactive = true;
-    public void Play(string CambiodeScena)
+    public static string dificultadSeleccionada;
+
+    public void Play()
     {
+        dificultadPanel.SetActive(true);
+        MenuPanel.SetActive(false);
+    }
+
+    public void SeleccionarDificultad(string dificultad)
+    {
+        dificultadSeleccionada = dificultad;
         SceneManager.LoadScene(CambiodeScena);
     }
 
     public void AbrirOpciones()
     {
-        if (menuisactive == true)
+        if (menuisactive)
         {
             MenuPanel.SetActive(false);
             opcionesPanel.SetActive(true);
-            menuisactive = false;
         }
         else
         {
             MenuPanel.SetActive(true);
             opcionesPanel.SetActive(false);
-            menuisactive = true;
         }
+        menuisactive = !menuisactive;
     }
 
     public void AbrirCreditos()
     {
-        if (menuisactive == true)
+        if (menuisactive)
         {
             MenuPanel.SetActive(false);
             creditosPanel.SetActive(true);
-            menuisactive = false;
         }
         else
         {
             MenuPanel.SetActive(true);
             creditosPanel.SetActive(false);
-            menuisactive = true;
         }
+        menuisactive = !menuisactive;
     }
 
     public void Salir()
